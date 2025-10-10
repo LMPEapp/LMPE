78f67ab56ad3bd6f4f1b9c2bb34a31f75c8dcd11
Maxence COEUR <maxence.chachane@gmail.com>
2025-10-10 14:52:16 +0200

    test noti

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterOutlet } from '@angular/router';
-import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { SwPush, SwUpdate, VersionReadyEvent } from '@angular/service-worker';
 import { filter } from 'rxjs/operators';
 
 @Component({
@@ -15,7 +15,7 @@ import { filter } from 'rxjs/operators';
 export class App {
   protected readonly title = signal('LMPE');
 
-  constructor(private swUpdate: SwUpdate, private snackBar: MatSnackBar) {
+  constructor(private swUpdate: SwUpdate, private snackBar: MatSnackBar, private swPush: SwPush) {
     if (this.swUpdate.isEnabled) {
       this.swUpdate.versionUpdates
         .pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
@@ -33,23 +33,10 @@ export class App {
           }, 1000); // 1 seconde avant reload pour voir le snackBar
         });
     }
-  }
-
-  ngOnInit() {
-    if ('serviceWorker' in navigator) {
-      // Ferme les notifications ouvertes
-      navigator.serviceWorker.getRegistration().then(reg => {
-        if (reg && reg.getNotifications) {
-          reg.getNotifications().then(notifications => {
-            notifications.forEach(n => n.close());
-          });
-        }
-      });
-
-      // Enregistre le custom service worker pour gérer les clics
-      navigator.serviceWorker.register('/assets/custom-service-worker.js')
-        .then(reg => console.log('Custom SW registered', reg))
-        .catch(err => console.error('SW registration failed', err));
-    }
+    this.swPush.notificationClicks.subscribe(event => {
+      console.log('Notification click:', event);
+      // ouvrir page spécifique
+      window.open(event.action === 'explore' ? '/home' : '/', '_blank');
+    });
   }
 }
